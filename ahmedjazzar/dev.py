625ebdf96b9e6a79608099d97745b7185b6c3722
ahmedjazzar <ahmed.mojaz@gmail.com>
2015-09-08 16:05:09 +0300

    Changing project static root.

--- ahmedjazzar/dev.py
+++ ahmedjazzar/dev.py
@@ -3,7 +3,8 @@ from .settings import *
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'ahmedjazzar-staticfiles')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '.compiled'),
     os.path.join(BASE_DIR, 'static'),
