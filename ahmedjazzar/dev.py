4ca27374c332707f60a206f52436332931b96442
ahmedjazzar <ahmed.mojaz@gmail.com>
2015-09-08 16:10:31 +0300

    Adding media root.

--- ahmedjazzar/dev.py
+++ ahmedjazzar/dev.py
@@ -3,9 +3,12 @@ from .settings import *
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'ahmedjazzar-staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'ahmedjazzar-staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '.compiled'),
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'ahmedjazzar-mediafiles')
