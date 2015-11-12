beff0a81ca599bf86255968d9dcf65706f522145
ahmedjazzar <ahmed.mojaz@gmail.com>
2015-11-13 01:42:22 +0200

    Finishing up iMac part.

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -7,6 +7,8 @@ var imacContainer = $( '#imac-container' );
 var navContainer = $( '#toggle' );
 var navOverlay = $( '#overlay' );
 var scrollTop = $( '#scroll-top' );
+var nightBackground = $( '#night-bg' );
+var dayBackground = $( '#day-bg' );
 
 var imageWidth = 430;
 var xp = 0;
@@ -17,6 +19,7 @@ var relMouseX = 0;
 var windowCenterX = 0;
 var timeInterval = 50;
 var loop = 0;
+var relativeWidth = 0;
 
 // iMac Animation
 imacContainer.hover(function(){
@@ -30,12 +33,18 @@ imacContainer.hover(function(){
   loop = setInterval(function(){
     xp += (relMouseX - xp) / timeInterval;
     xp = Math.max(xpLow, Math.min(xp, xpHigh));
+    relativeWidth = (imageWidth + xp) * 0.5;
+
     nightImg.css({width: (imageWidth + xp) * 0.5, left: (imageWidth - xp) * 0.5});
+    nightBackground.css({opacity: relativeWidth/imageWidth});
+    dayBackground.css({opacity: 1 - (relativeWidth/imageWidth)});
   });
 }, function(){
   clearInterval(loop);
   xp = 0;
   nightImg.animate({width: imageWidth * 0.5, left: imageWidth * 0.5}, timeInterval);
+  nightBackground.css({opacity: 1});
+  dayBackground.css({opacity: 1});
 });
 
 // toggle nav menu
