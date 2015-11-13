d26fa13408f98a82125f5ce5a57c2e8f258d5b17
ahmedjazzar <ahmed.mojaz@gmail.com>
2015-11-13 14:48:41 +0200

    Adding enhancements on iMac.

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -9,6 +9,7 @@ var navOverlay = $( '#overlay' );
 var scrollTop = $( '#scroll-top' );
 var nightBackground = $( '#night-bg' );
 var dayBackground = $( '#day-bg' );
+var imac = $( '#imac' );
 
 var imageWidth = 430;
 var xp = 0;
@@ -17,7 +18,7 @@ var xpLow = -imageWidth;
 var mouseX = 0;
 var relMouseX = 0;
 var windowCenterX = 0;
-var timeInterval = 50;
+var timeInterval = 500;
 var loop = 0;
 var relativeWidth = 0;
 
@@ -30,14 +31,18 @@ imacContainer.hover(function(){
   });
 
   nightImg.stop();
+  imac.stop();
   loop = setInterval(function(){
-    xp += (relMouseX - xp) / timeInterval;
+    xp += 10 * (relMouseX - xp) / timeInterval;
     xp = Math.max(xpLow, Math.min(xp, xpHigh));
     relativeWidth = (imageWidth + xp) * 0.5;
 
     nightImg.css({width: (imageWidth + xp) * 0.5, left: (imageWidth - xp) * 0.5});
-    nightBackground.css({opacity: relativeWidth/imageWidth});
-    dayBackground.css({opacity: 1 - (relativeWidth/imageWidth)});
+    imac.css({right: xp/25});
+
+    var relativeOpacity = 2 * relativeWidth/imageWidth;
+    nightBackground.css({opacity: relativeOpacity});
+    dayBackground.css({opacity: 2 - relativeOpacity});
   });
 }, function(){
   clearInterval(loop);
@@ -45,6 +50,7 @@ imacContainer.hover(function(){
   nightImg.animate({width: imageWidth * 0.5, left: imageWidth * 0.5}, timeInterval);
   nightBackground.css({opacity: 1});
   dayBackground.css({opacity: 1});
+  imac.animate({right: 0}, timeInterval);
 });
 
 // toggle nav menu
