c1ab3c9a682ffe20fcd5de11f590122a2d54cae3
Ahmed Jazzar <me@ahmedjazzar.com>
2018-01-16 23:36:14 +0200

    S3 to CloudFront

--- ahmedjazzar/prod.py
+++ ahmedjazzar/prod.py
@@ -29,12 +29,11 @@ SITE_BASE = 'www.ahmedjazzar.com'
 
 
 # Static and Media asset configuration
-
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 'd3ffm3ou81sw69.cloudfront.net'
 
 
 STATIC_FILES_LOCATION = 'static'
