33260e92a477068a6117060fff468d86aa9110b7
Ahmed Jazzar <me@ahmedjazzar.com>
2019-03-22 01:55:16 -0700

    Add pagination to API

--- ahmedjazzar/common.py
+++ ahmedjazzar/common.py
@@ -64,8 +64,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
